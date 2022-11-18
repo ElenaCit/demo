73d834d2f8586fae73c0d3f5547f3e551f0c1f7c
e232714 <e232714@sbl.ch>
2022-11-18 08:30:09 +0100

    cannot fall out of window

--- script.js
+++ script.js
@@ -3,23 +3,29 @@ let dy = 0
 
 
 function handlekeyPress (ev) {
-    const player = document.querySelector ("#player")
+    const player = document.querySelector ("#player");
     if (ev.code ==="Space") {
         dy = -1
     } else if (ev.key === "r"){
-        player.setAttribute("fill", "red")
+        player.setAttribute("fill", "red");
     }else if (ev.key === "g"){
-        player.setAttribute("fill", "green")
+        player.setAttribute("fill", "green");
     }
 }
 
 function gameLoop(){
-    // Update game status
+    // Update game status 
     dy = Math.min(dy + 0.05, 1)
 
 
-    const player = document.querySelector("#player")
-let posY = parseFloat (player.getAttribute ("cy"))
+
+const player = document.querySelector("#player");
+let posY = parseFloat (player.getAttribute ("cy"));
+let r = parseInt(player.getAttribute("r"))
+if (posY > 100 - r) {
+    dy = 0 
+}
+
 player.setAttribute("cy", posY + dy);
 
 
